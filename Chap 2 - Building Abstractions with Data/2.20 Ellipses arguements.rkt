5d5e49e62735cacf44e607a065a441af5932a332
squarebat <ksidhpuri@gmail.com>
2021-06-22 10:41:58 +0530

    add: chap 2 exercises

--- Chap 2 - Building Abstractions with Data/2.20 Ellipses arguements.rkt
+++ Chap 2 - Building Abstractions with Data/2.20 Ellipses arguements.rkt
@@ -1,8 +1,14 @@
 #lang sicp
-(define (odd? x) (= (remainder 2) 1))
-(define (even? x) (not (odd? x)))
-(define (same-parity . w)
-  (define (parity? x))
-   (if (null? items)
-      nil
-      (append (reverse (cdr items)) (list (car items)))))
+(define (same-parity first . items)
+  (define (parity x) (remainder x 2))
+  (define (eq-parity? x y) (= (parity x) (parity y)))
+  (define (iter items result)
+    (cond ((null? items) (reverse result))
+        ((eq-parity? first (car items))
+         (iter (cdr items) (cons (car items) result)))
+        (else (iter (cdr items) result)))
+  )
+  (iter items (list first))
+)
+  
+(same-parity 1 2 3 4 5 6 7)
---
+++ Chap 2 - Building Abstractions with Data/2.20 Ellipses arguements.rkt~
@@ -0,0 +1,8 @@
+#lang sicp
+(define (odd? x) (= (remainder 2) 1))
+(define (even? x) (not (odd? x)))
+(define (same-parity . w)
+  (define (parity? x))
+   (if (null? items)
+      nil
+      (append (reverse (cdr items)) (list (car items)))))
