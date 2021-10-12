04d787ccf39b031e472424da8a6a72828d134dbe
squarebat <ksidhpuri@gmail.com>
2021-10-12 19:40:43 +0530

    complete: chap 3. YEAAAA CHAP 4 LESSS GOOOOOOOOOOOO

---
+++ Chap 3 - Modularity, Objects, and State/3.78 second order differential.scm
@@ -0,0 +1,5 @@
+(define (solve-2nd a b y0 dy0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams (scale-stream dy a) (scale-stream y b)))
+  y)
---
+++ Chap 3 - Modularity, Objects, and State/utility functions.scm
@@ -0,0 +1,28 @@
+;utility functions
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-map-mul proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map-mul
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
